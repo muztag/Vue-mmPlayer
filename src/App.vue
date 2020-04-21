57f40333b91561abbf4ac30526927b353e437410
muztag <mail@wangkunlun.win>
2020-04-22 03:03:17 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -59,10 +59,10 @@ export default {
     let titleTime
     document.addEventListener('visibilitychange', function() {
       if (document.hidden) {
-        document.title = '死鬼去哪里了！'
+        document.title = '躲猫猫'
         clearTimeout(titleTime)
       } else {
-        document.title = '(つェ⊂)咦!又好了!'
+        document.title = '回来了'
         titleTime = setTimeout(function() {
           document.title = OriginTitile
         }, 2000)
