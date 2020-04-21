4fdc4a3cd22230119f63d7c44a53c5dd9ba06ebc
muztag <mail@wangkunlun.win>
2020-04-22 03:33:21 +0800

    1

--- src/components/music-btn/music-btn.vue
+++ src/components/music-btn/music-btn.vue
@@ -20,7 +20,6 @@
           v-show="!showInput"
           type="number"
           autofocus
-          placeholder="这里输入对方编号同步对方歌曲"
           value="2178530"
           v-model.trim="dest"
           @keyup.enter="submit"
